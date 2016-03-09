867986c47689c3a56fce04ff171cf5026fa6c4ee
Dzianis Spartak <dspartak@edmunds.com>
2016-03-09 23:21:35 +0300

    Added material-ui components

--- client/js/views/notFound/NotFound.jsx
+++ client/js/views/notFound/NotFound.jsx
@@ -1,17 +1,20 @@
 'use strict';
 
 let React = require('react');
+let Card = require('material-ui/lib/card/card');
+let CardTitle = require('material-ui/lib/card/card-title');
+let CardText = require('material-ui/lib/card/card-text');
 let {Link} = require('react-router');
 
 let NotFound = React.createClass({
     render() {
         return (
-            <div>
-                <h1>404 error</h1>
-                <h2>page not found</h2>
-                <p>We are sorry but the page you are looking for does not exist<br />
-                    You could return to the <Link to="/main">homepage</Link></p>
-            </div>
+            <Card>
+                <CardTitle title="404 error" subtitle="page not found" />
+                <CardText>We are sorry but the page you are looking for does not exist<br />
+                    You could return to the <Link to="/main">homepage</Link>
+                </CardText>
+            </Card>
         );
     }
 });
