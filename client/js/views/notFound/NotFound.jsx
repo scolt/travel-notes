a2cfced5ab386ac48568e919f9d1e27951e83466
Dzianis Spartak <dspartak@edmunds.com>
2016-03-10 10:05:14 +0300

    #13 Fixed pome page link path

--- client/js/views/notFound/NotFound.jsx
+++ client/js/views/notFound/NotFound.jsx
@@ -12,7 +12,7 @@ let NotFound = React.createClass({
             <Card>
                 <CardTitle title="404 error" subtitle="page not found" />
                 <CardText>We are sorry but the page you are looking for does not exist<br />
-                    You could return to the <Link to="/main">homepage</Link>
+                    You could return to the <Link to="#/main">homepage</Link>
                 </CardText>
             </Card>
         );
