2fec2be4dc889ef9dc4d94cf43d53cfbe385eb36
Andjey <andjey@tut.by>
2016-03-28 15:23:16 +0000

    fix merge conflict

--- client/js/views/map/Map.jsx
+++ client/js/views/map/Map.jsx
@@ -16,7 +16,7 @@ let MapPage = React.createClass({
     render() {
         return (
             <div className="row">
-                {mapInfo.isFetching ? <div className="spinner"><Icon name="circle-o-notch" spin/></div> : <Map markers={markers} type="fullscreen"/>}
+                {this.state.net.isFetching ? <div className="spinner"><Icon name="circle-o-notch" spin/></div> : <Map markers={this.state.notes} type="fullscreen"/>}
             </div>
         );
     }
