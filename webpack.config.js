aade95b8f6a47f5cf616c069ad8bfda74ff72c9b
Viktar Parashchanka <gxvint@gmail.com>
2016-10-17 22:50:04 +0300

    Fixes by comments

--- webpack.config.js
+++ webpack.config.js
@@ -87,12 +87,8 @@ const config = {
                 loader: ExtractTextPlugin.extract('style', 'css')
             },
             {
-                test: /favicon\.png\??/,
-                loader: 'url?limit=1&name=[path][name].[ext]'
-            },
-            {
                 test: /\.(otf|eot|svg|ttf|woff|png|jpg)\??/,
-                loader: 'url?limit=20000&name=[path][name].[ext]'
+                loader: 'url?limit=100&name=[path][name].[ext]'
             },
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
