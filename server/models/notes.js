02418c2a22153237f24b35b22455fed8ffa24e09
Viktar Parashchanka <gxvint@gmail.com>
2016-10-18 12:27:35 +0300

    Updated by comments

--- server/models/notes.js
+++ server/models/notes.js
@@ -63,10 +63,7 @@ let NoteActions = (function () {
     }
 
     function saveNote(data, user, image, cb) {
-        data.photo = image;
-        data.userId = user;
-        data.isDraft = false;
-        data.isDel = false;
+        data = {...data, photo: image, userId: user, isDraft: false, isDel: false};
         Note.create(data, (err, note) => {
             cb(err, note);
         })
