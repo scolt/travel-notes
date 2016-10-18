b08a96d11e833212c56b8db2c0b87d47053e4084
Viktar Parashchanka <gxvint@gmail.com>
2016-10-18 12:21:16 +0300

    Updated config for electron

--- _native/electron/main.js
+++ _native/electron/main.js
@@ -1,16 +1,17 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const windowProperties = {
+    width: 800,
+    height: 600,
+    resizable: false,
+    icon: __dirname + '/www/icon.png'
+};
 
 let mainWindow = null;
 app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        resizable: false,
-        icon: __dirname + '/www/icon.png'
-    });
+    mainWindow = new BrowserWindow(windowProperties);
     mainWindow.loadURL('file://' + __dirname + '/www/index.html');
     mainWindow.setMenu(null);
     mainWindow.webContents.executeJavaScript(`startApp()`);
