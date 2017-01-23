33664cf8f7bc8f7a71eb01e739b4daa0d11626b5
Viktar Parashchanka <gxvint@gmail.com>
2017-01-23 15:45:58 +0300

    #59 Reverted unexpected changes

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,15 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StringReplacePlugin = require('string-replace-webpack-plugin');
 
 const definitions = new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production' || 'development')
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
 });
 
-const isDev = false;
+const isDev = JSON.parse(definitions.definitions['process.env.NODE_ENV']) == 'development';
 
 const replacements = [
     {
         pattern: /\$\$REST_URL_HOST/ig,
-        replacement: () => process.env.remoteServer || (isDev ? 'http://iwasthere.herokuapp.com' : 'http://iwasthere.herokuapp.com')
+        replacement: () => process.env.remoteServer || (isDev ? 'http://iwasthere.herokuapp.com' : '')
     }
 ];
 
