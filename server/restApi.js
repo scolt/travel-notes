8678c5f0cdee8271ccad223a304383e643433915
Viktar Parashchanka <gxvint@gmail.com>
2017-01-23 17:12:04 +0300

    #59 fixed bug for microsoft edge

--- server/restApi.js
+++ server/restApi.js
@@ -14,7 +14,7 @@ function restApi(req, res, next) {
     let action = req.params.action;
 
     let type = 'logged';
-    if (!req.user) type = req.header('Authorization').includes('Bearer') ? 'expired' : 'guest';
+    if (!req.user) type = req.header('Authorization') && req.header('Authorization').includes('Bearer') ? 'expired' : 'guest';
     res.header('tn-user-type', type);
 
     if (!models[model] || !models[model][action]) return next(new Error('Service or action not extists'));
