304a4f159f6ba283c6aae69fc05f55861fda0f9c
Dzmitry.Shaplyka <dzmitry_shaplyka@epam.com>
2017-02-24 18:28:14 +0300

    Changed E2E test sctructure

---
+++ tests/e2e/pageObjects/homePage.js
@@ -0,0 +1,9 @@
+export class HomePage {
+
+    //Elements
+    getStartedButton = "div[data-reactid='.0.1.0.0.0.2.0.0']";
+
+
+
+
+}
---
+++ tests/e2e/specs/homePage.spec.js
@@ -0,0 +1,24 @@
+import {HomePage} from '../pageObjects/homePage.js';
+import {expect} from 'chai';
+
+describe('I was there first test', function() {
+    const home = new HomePage();
+
+    before(() => {
+        browser.url("/");
+    });
+
+    it('Click register button', function() {
+
+        browser.click(home.getStartedButton);
+
+    });
+
+    it('Check', function() {
+
+        browser.click(home.getStartedButton);
+
+    });
+
+
+});
