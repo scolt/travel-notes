52ff96fc8e51e6e3c031a27b131acc1a1401fea9
Viktar Parashchanka <gxvint@gmail.com>
2017-02-28 13:40:27 +0300

    Notes grid fixed

--- client/containers/Note/View/components/addImage/ComponentAddImage.jsx
+++ client/containers/Note/View/components/addImage/ComponentAddImage.jsx
@@ -27,13 +27,15 @@ const AddImageButton = React.createClass({
     render() {
         const addImageFormField = this.props.data.notes.addImageForm.fields[0];
         return <Dropzone onDrop={this.onDrop}
-                         className="col-33-custom gallery-item drop-zone"
+                         className="col-33-custom"
                          activeClassName="active"
                          accept="image/*">
-            <div className="drop-zone-sq">
-                <strong>Add New Image</strong>
-                {addImageFormField.value ? <img src={addImageFormField.value.preview}/> : null}
-                {addImageFormField.value ? <div className="submit-drop" onTouchTap={this.uploadImage}>Upload</div> : null}
+            <div className="gallery-item drop-zone">
+                <div className="drop-zone-sq">
+                    <strong>Add New Image</strong>
+                    {addImageFormField.value ? <img src={addImageFormField.value.preview}/> : null}
+                    {addImageFormField.value ? <div className="submit-drop" onTouchTap={this.uploadImage}>Upload</div> : null}
+                </div>
             </div>
         </Dropzone>;
     }
