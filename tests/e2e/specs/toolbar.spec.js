0f02dda129c936562d80e3214516e28d101e0c89
Dzmitry.Shaplyka <dmitryshaplyko@gmail.com>
2017-03-10 23:39:16 +0300

    Test for absence of Sign in And login buttons added

--- tests/e2e/specs/toolbar.spec.js
+++ tests/e2e/specs/toolbar.spec.js
@@ -1,5 +1,8 @@
 import {Toolbar} from "../pageObjects/common/toolbar";
 import {HomePage} from '../pageObjects/homePage.js';
+import {LoginPage} from '../pageObjects/loginPage';
+import {MainPage} from "../pageObjects/mainPage";
+import {consts} from '../consts';
 import {expect} from 'chai';
 import steps from '../steps/actionSteps'
  
@@ -8,6 +11,8 @@ import steps from '../steps/actionSteps'
 
     const home = new HomePage();
     const toolbar = new Toolbar();
+    const login = new LoginPage();
+    const main = new MainPage();
 
         before(() => {
             steps.navigateTo(home.url);
@@ -17,11 +22,18 @@ import steps from '../steps/actionSteps'
             expect(browser.isVisible(toolbar.menuButton)).to.be.true;
         });
         
-        it('Should contain login button', () => {
+        it('Should contain login button for non Authorized users', () => {
             expect(browser.isVisible(toolbar.logInButton)).to.be.true;    
         });
         
-        it('Should contain Sign In button', () => {
+        it('Should contain Sign In button for Non Authorized users', () => {
             expect(browser.isVisible(toolbar.signUpButton)).to.be.true;
+        });
+
+        it('Should not contain Sign In and Login buttons for Authorized users', () => {
+            login.login(consts.username, consts.password);
+            steps.waitForPageisLoaded(main.url);
+            expect(browser.isVisible(toolbar.logInButton)).to.be.false;
+            expect(browser.isVisible(toolbar.signUpButton)).to.be.false;
         });
 });
